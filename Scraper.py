961df0d9f7c2d3fe15028f9d2ae11f3a226e5e58
traviswu <travis.wu.bmc@gmail.com>
2016-05-23 02:51:46 -0700

    first commit

---
+++ Scraper.py
@@ -0,0 +1,31 @@
+#Spider Web Crawler 
+#http://www.carfax.com/
+#https://www.manheim.com/
+
+import urlparse
+import urllib
+from bs4 import BeautifulSoup
+
+url = "http://traviswu.me"
+
+urls = [url] #stack of urls to scrape 
+visited = [url] #historic record of urls 
+
+while len(urls) > 0:
+	try: 
+		htmltext = urllib.urlopen(urls[0]).read() 
+	except: 
+		print urls[0]
+
+	soup = BeautifulSoup(htmltext,"html.parser")
+	urls.pop(0)
+	print len(urls)
+
+	#print soup.findAll('a',href=True)
+	for tag in soup.findAll('a',href=True):
+		tag['href'] = urlparse.urljoin(url,tag['href'])
+		if url in tag['href'] and tag['href'] not in visited:
+			urls.append(tag['href'])
+			visited.append(tag['href'])
+
+print visited 
